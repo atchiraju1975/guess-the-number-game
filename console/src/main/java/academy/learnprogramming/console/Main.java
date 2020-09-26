576adb305caa9d9814a9394d7ac6812bed262d77
SrinivasaChakka <Srinivasa.Chakka@tesco.com>
2020-09-26 07:46:54 +0530

    lombok setup corrected

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -1,14 +1,17 @@
 package academy.learnprogramming.console;
 import academy.learnprogramming.config.GameConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.invoke.MethodHandles;
+
 @Slf4j
 public class Main {
 
-  //  private static final Logger log = LoggerFactory.getLogger(Main.class);
+ //   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void main(String[] args) {
 
